fec5619d6c1eb4fd4c6bb966384e87cf53878231
Israel Valverde <israelpereiravalverde@gmail.com>
2016-03-31 20:25:37 -0300

    Grocery items are updated and saved to database

--- src/Server/GroceryListAppWebApi/Controllers/GroceryItemController.cs
+++ src/Server/GroceryListAppWebApi/Controllers/GroceryItemController.cs
@@ -1,10 +1,9 @@
-﻿using GroceryListAppWebApi.Models;
+﻿using GroceryListAppWebApi.DTO;
+using GroceryListAppWebApi.Resources;
 using GroceryListAppWebApi.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace GroceryListAppWebApi.Controllers
@@ -19,27 +18,65 @@ namespace GroceryListAppWebApi.Controllers
         }
 
         // GET api/<controller>
-        public IEnumerable<GroceryItem> Get()
+        public IEnumerable<GroceryItemDto> Get()
         {
             return this._groceryItemService.GetGroceryItems();
         }
 
         // GET api/<controller>/5
-        public string Get(int id)
+        public GroceryItemDto Get(int id)
         {
-            throw new NotImplementedException("This action is not implemented yet.");
+            return this._groceryItemService.GetById(id);
         }
 
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody]GroceryItemDto groceryItem)
         {
-            throw new NotImplementedException("This action is not implemented yet.");
+            object result;
+            if(this.ModelState.IsValid)
+            {
+                this._groceryItemService.Add(groceryItem);
+                result = new
+                {
+                    Success = true,
+                    Message = MessageResource.GroceryItemSuccessfullySaved
+                };
+            }
+            else
+            {
+                result = new
+                {
+                    Success = false,
+                    Errors = this.ModelState.Where(kv => kv.Value.Errors.Any())
+                };
+            }
+
+            return Ok(result);
         }
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        // PUT api/<controller>
+        public IHttpActionResult Put([FromBody]GroceryItemDto groceryItem)
         {
-            throw new NotImplementedException("This action is not implemented yet.");
+            object result;
+            if (this.ModelState.IsValid)
+            {
+                this._groceryItemService.Update(groceryItem);
+                result = new
+                {
+                    Success = true,
+                    Message = MessageResource.GroceryItemSuccessfullySaved
+                };
+            }
+            else
+            {
+                result = new
+                {
+                    Success = false,
+                    Errors = this.ModelState.Where(kv => kv.Value.Errors.Any())
+                };
+            }
+
+            return Ok(result);
         }
 
         // DELETE api/<controller>/5

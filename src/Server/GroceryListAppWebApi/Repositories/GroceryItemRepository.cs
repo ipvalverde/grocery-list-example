a882f4b9fac581bc8a34ed14e4d6c14c6f52f08b
Israel Valverde <israelpereiravalverde@gmail.com>
2016-03-31 20:27:18 -0300

    Grocery items are updated and saved to database

--- src/Server/GroceryListAppWebApi/Repositories/GroceryItemRepository.cs
+++ src/Server/GroceryListAppWebApi/Repositories/GroceryItemRepository.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace GroceryListAppWebApi.Repositories
 {
@@ -23,6 +22,60 @@ namespace GroceryListAppWebApi.Repositories
         {
             // TODO filter grocery items by current logged user
             return this._context.GroceryItems.ToList();
+        }
+
+        /// <summary>
+        /// Get grocery item with the given id.
+        /// </summary>
+        /// <returns>Returns the grocery item with the given id, or null if it does not exists.</returns>
+        public GroceryItem GetGroceryItem(long itemId)
+        {
+            // TODO filter grocery items by current logged user
+            return this._context.GroceryItems.Find(itemId);
+        }
+
+        /// <summary>
+        /// Creates a new grocery item with the given name and bought status.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="bought"></param>
+        public void Create(string name, bool bought)
+        {
+            this._context.GroceryItems.Add(new GroceryItem
+            {
+                Name = name,
+                Bought = bought,
+                DateTimeCreated = DateTime.UtcNow
+            });
+        }
+
+        /// <summary>
+        /// Set the given grocery item as modified.
+        /// </summary>
+        /// <param name="groceryItem"></param>
+        public void Update(GroceryItem groceryItem)
+        {
+            // TODO check if the item being updated is owned by the current logged user
+            this._context.Entry(groceryItem).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Delete the grovery item with the given id.
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(long id)
+        {
+            GroceryItem item = new GroceryItem() { Id = id };
+            this._context.GroceryItems.Attach(item);
+            this._context.GroceryItems.Remove(item);
+        }
+
+        /// <summary>
+        /// Apply the changes performed over this repository instance.
+        /// </summary>
+        public void SaveChanges()
+        {
+            this._context.SaveChanges();
         }
     }
 }
--- src/Server/GroceryListAppWebApi/Repositories/IGroceryItemRepository.cs
+++ src/Server/GroceryListAppWebApi/Repositories/IGroceryItemRepository.cs
@@ -10,5 +10,35 @@ namespace GroceryListAppWebApi.Repositories
         /// </summary>
         /// <returns>Returns a IEnumerable of grocery items.</returns>
         IEnumerable<GroceryItem> GetGroceryItems();
+
+        /// <summary>
+        /// Get grocery item with the given id.
+        /// </summary>
+        /// <returns>Returns the grocery item with the given id, or null if it does not exists.</returns>
+        GroceryItem GetGroceryItem(long itemId);
+
+        /// <summary>
+        /// Creates a new grocery item with the given name and bought status.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="bought"></param>
+        void Create(string name, bool bought);
+
+        /// <summary>
+        /// Set the given grocery item as modified.
+        /// </summary>
+        /// <param name="groceryItem"></param>
+        void Update(GroceryItem groceryItem);
+
+        /// <summary>
+        /// Delete the grovery item with the given id.
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(long id);
+
+        /// <summary>
+        /// Apply the changes performed over this repository instance.
+        /// </summary>
+        void SaveChanges();
     }
 }
